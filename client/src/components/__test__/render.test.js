1b53365667b997ca775dd2097928bb3189684567
andrewchester1 <49591047+andrewchester1@users.noreply.github.com>
2021-07-06 12:13:53 -0500

    unit testing

--- client/src/components/__test__/render.test.js
+++ client/src/components/__test__/render.test.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
-import { Provider } from 'react-dom'
-import { useSelector } from 'react-redux';
-import useStyles from '../Posts/styles';
-
-import Adapter from 'enzyme-adapter-react-16';
-Enzyme.configure({adapter: new Adapter()});
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+import renderer from 'react-test-renderer';
 
 import Posts from '../Posts/Posts';
 
-it("renders without crashing 1", () => {
-  // const posts = useSelector((state) => state.posts);
-  // const classes = useStyles();
+const mockStore = configureStore([]);
 
-  shallow(<Provider><Posts /></ Provider>);
+describe('My Connected React-Redux Component', () => {
+  let store;
+  let component;
+
+  beforeEach(() => {
+    store = mockStore({
+      state: 'sample text'
+    });
+
+    component = renderer.create(
+    <Provider store={store}>
+      <Posts />
+    </Provider>
+    );
+  });
+    it("renders without crashing 1", () => {
+      expect(component.toJSON()).toMatchSnapshot();
+  });
 });
